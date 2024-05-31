e33fbcaae90b98c125ff164680993fec7f81403f
nivethamei <144530250+nivethamei@users.noreply.github.com>
2024-05-31 15:23:00 +0530

    pom class added

---
+++ GUISeleniumFramework/src/main/java/com/comcast/crm/objectrepositoryutility/CreateCampaignPage.java
@@ -0,0 +1,33 @@
+package com.comcast.crm.objectrepositoryutility;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class CreateCampaignPage {
+	/**
+	 * This class contains Create Campaign page elements
+	 * @param driver
+	 */
+	
+	public CreateCampaignPage(WebDriver driver) {
+		PageFactory.initElements(driver,this);
+	}
+	
+	@FindBy(name = "campaignname")
+	private WebElement campaignname;
+	
+	@FindBy(xpath = "//input[@title='Save [Alt+S]']")
+	private WebElement savebutton;
+
+	public WebElement getSavebutton() {
+		return savebutton;
+	}
+
+	public WebElement getCampaignname() {
+		return campaignname;
+	}
+
+
+}
