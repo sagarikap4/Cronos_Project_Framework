1210a029b5754df7960b743523e3a173d0b94d16
nivethamei <144530250+nivethamei@users.noreply.github.com>
2024-06-02 16:32:24 +0530

    Update SampleTest.java

--- GUISeleniumFramework/src/test/java/com/comcast/crm/contacttest/SampleTest.java
+++ GUISeleniumFramework/src/test/java/com/comcast/crm/contacttest/SampleTest.java
@@ -12,7 +12,7 @@ public class SampleTest extends BaseClass {
 
 	@Test(groups = {"smokeTest"})
 	public void createConTactTest() throws Throwable {		
-		/* read testScritp data from Excel file*/
+		/* read testScritp data from Excel file  */
 		String lastName = eLib.getDataFromExcel("contact", 1, 2) + jLib.getRandomNumber();
 
 	
