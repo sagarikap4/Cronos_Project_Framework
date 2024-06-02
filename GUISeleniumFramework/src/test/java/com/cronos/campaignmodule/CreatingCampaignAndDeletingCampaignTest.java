db6cd1b04757da86dda8daad35efa0a2fc3e2fe8
nivethamei <144530250+nivethamei@users.noreply.github.com>
2024-06-02 21:36:22 +0530

    latest changes

--- GUISeleniumFramework/src/test/java/com/cronos/campaignmodule/CreatingCampaignAndDeletingCampaignTest.java
+++ GUISeleniumFramework/src/test/java/com/cronos/campaignmodule/CreatingCampaignAndDeletingCampaignTest.java
@@ -13,6 +13,7 @@ import com.comcast.crm.objectrepositoryutility.CampaignInformationPage;
 import com.comcast.crm.objectrepositoryutility.CampaignPage;
 import com.comcast.crm.objectrepositoryutility.CreateCampaignPage;
 import com.comcast.crm.objectrepositoryutility.HomePage;
+
 @Listeners(com.comcast.crm.generic.listenerutility.ListImpClass.class)
 public class CreatingCampaignAndDeletingCampaignTest extends BaseClass {
 
@@ -27,25 +28,29 @@ public class CreatingCampaignAndDeletingCampaignTest extends BaseClass {
 		wutil.waitForPageToLoad(driver);
 		driver.manage().window().maximize();
 		System.out.println("browser maximized successfully");
-		//UtilityClassObject.getTest().log(Status.INFO, "browser maximized successfully");
+		// UtilityClassObject.getTest().log(Status.INFO, "browser maximized
+		// successfully");
 		HomePage hp = new HomePage(driver);
 		hp.getMoreLink().click();
 		hp.getCampaignsLink().click();
 		System.out.println("campaign page displayed successfully");
-		//UtilityClassObject.getTest().log(Status.INFO, "campaign page displayed successfully");
+		// UtilityClassObject.getTest().log(Status.INFO, "campaign page displayed
+		// successfully");
 
 		// Click on Campaign icon in the campaign page
 		CampaignPage cp = new CampaignPage(driver);
 		cp.getCampaignicon().click();
 		System.out.println("creating campaign page is displayed successfully");
-		//UtilityClassObject.getTest().log(Status.INFO, "creating campaign page is displayed successfully");
+		// UtilityClassObject.getTest().log(Status.INFO, "creating campaign page is
+		// displayed successfully");
 
 		// Creating a campaign name and click on save
 		CreateCampaignPage ccp = new CreateCampaignPage(driver);
 		ccp.getCampaignname().sendKeys(CAMPAIGNNAME);
 		ccp.getSavebutton().click();
 		System.out.println("creating information page is displayed successfully");
-		//UtilityClassObject.getTest().log(Status.INFO, "creating information page id displayed successfully");
+		// UtilityClassObject.getTest().log(Status.INFO, "creating information page id
+		// displayed successfully");
 
 		// verify the header msg
 		CampaignInformationPage cip = new CampaignInformationPage(driver);
@@ -57,28 +62,27 @@ public class CreatingCampaignAndDeletingCampaignTest extends BaseClass {
 		String actcampaignname = cip.getVerifycampaignname().getText();
 		System.out.println(actcampaignname);
 		Assert.assertTrue(actcampaignname.contains(CAMPAIGNNAME));
-        System.out.println("verified the campaignname");
-        
-        //click on campaign link in header tab
-         hp.getMoreLink().click();
-         hp.getCampaignsLink().click();
-         System.out.println("campaign page is displayed");
-         
-         //enter the campaign name in textfield and search
-          cp.getSearchtextfield().sendKeys(CAMPAIGNNAME);
-          WebElement searchdropdown = cp.getSearchindropdown();
-          wutil.select(searchdropdown,DROPDOWN);
-          cp.getSearchnowbutton().click();
-          System.out.println("search is done");
-          
-          //click on dellink
-          cp.getDellink().click();
-          System.out.println("click on del link");
-   
-          
-          wutil.switchtoAlertAndAccept(driver);
-          System.out.println("pop up is handled");
-     
+		System.out.println("verified the campaignname");
+
+		// click on campaign link in header tab
+		hp.getMoreLink().click();
+		hp.getCampaignsLink().click();
+		System.out.println("campaign page is displayed");
+
+		// enter the campaign name in textfield and search
+		cp.getSearchtextfield().sendKeys(CAMPAIGNNAME);
+		WebElement searchdropdown = cp.getSearchindropdown();
+		wutil.select(searchdropdown, DROPDOWN);
+		cp.getSearchnowbutton().click();
+		System.out.println("search is done");
+
+		// click on dellink
+		cp.getDellink().click();
+		System.out.println("click on del link");
+
+		wutil.switchtoAlertAndAccept(driver);
+		System.out.println("pop up is handled");
+
 	}
 
 }
--- GUISeleniumFramework/src/test/java/com/cronos/campaignmodule/CreatingCampaignWithEventsTest.java
+++ GUISeleniumFramework/src/test/java/com/cronos/campaignmodule/CreatingCampaignWithEventsTest.java
@@ -16,94 +16,83 @@ import com.comcast.crm.objectrepositoryutility.CampaignInformationPage;
 import com.comcast.crm.objectrepositoryutility.CampaignPage;
 import com.comcast.crm.objectrepositoryutility.CreateCampaignPage;
 import com.comcast.crm.objectrepositoryutility.HomePage;
+
 @Listeners(com.comcast.crm.generic.listenerutility.ListImpClass.class)
-public class CreatingCampaignWithEventsTest extends BaseClass{
-	
-	
+public class CreatingCampaignWithEventsTest extends BaseClass {
+
 	@Test
 	public void CreatingCampaignWithEventsTest() throws Throwable {
-		
-		WebDriverUtility wutil=new WebDriverUtility();
-		
-		String CAMPAIGNNAME = eLib.getDataFromExcel("CampaignModule",1 , 2);
+
+		WebDriverUtility wutil = new WebDriverUtility();
+
+		String CAMPAIGNNAME = eLib.getDataFromExcel("CampaignModule", 1, 2);
 		String EVENTNAME = eLib.getDataFromExcel("CampaignModule", 1, 3);
 		String TIME = eLib.getDataFromExcel("CampaignModule", 1, 4);
-		
-		//click on morelink and click on campaign
+
+		// click on morelink and click on campaign
 		wutil.waitForPageToLoad(driver);
 		driver.manage().window().maximize();
 		UtilityClassObject.getTest().log(Status.INFO, "browser maximized successfully");
-		HomePage hp=new HomePage(driver);
+		HomePage hp = new HomePage(driver);
 		hp.getMoreLink().click();
 		hp.getCampaignsLink().click();
 		UtilityClassObject.getTest().log(Status.INFO, "campaign page displayed successfully");
-		
-		
-		//Click on Campaign icon in the campaign page
-		CampaignPage cp=new CampaignPage(driver);
+
+		// Click on Campaign icon in the campaign page
+		CampaignPage cp = new CampaignPage(driver);
 		cp.getCampaignicon().click();
 		UtilityClassObject.getTest().log(Status.INFO, "creating campaign page is displayed successfully");
-		
-		//Creating a campaign name and click on save
-		CreateCampaignPage ccp=new CreateCampaignPage(driver);
+
+		// Creating a campaign name and click on save
+		CreateCampaignPage ccp = new CreateCampaignPage(driver);
 		ccp.getCampaignname().sendKeys(CAMPAIGNNAME);
 		ccp.getSavebutton().click();
 		UtilityClassObject.getTest().log(Status.INFO, "creating information page id displayed successfully");
-		
-	    //verify the header msg
-		CampaignInformationPage cip=new CampaignInformationPage(driver);
+
+		// verify the header msg
+		CampaignInformationPage cip = new CampaignInformationPage(driver);
 		String actheadermsg = cip.getVerifycampaignheadermsg().getText();
 		System.out.println(actheadermsg);
 		Assert.assertTrue(actheadermsg.contains(CAMPAIGNNAME));
-		
-		//verify the campaign name
+
+		// verify the campaign name
 		String actcampaignname = cip.getVerifycampaignname().getText();
 		System.out.println(actcampaignname);
-	    Assert.assertTrue(actcampaignname.contains(CAMPAIGNNAME));
-	    
-	    //click on events link
+		Assert.assertTrue(actcampaignname.contains(CAMPAIGNNAME));
+
+		// click on events link
 		cip.getAddevents().click();
 		UtilityClassObject.getTest().log(Status.INFO, "creating events page is displayed successfully");
 
-		//Creating a events in creatingevent page
-		CampaignEventsPage cep=new CampaignEventsPage(driver);
+		// Creating a events in creatingevent page
+		CampaignEventsPage cep = new CampaignEventsPage(driver);
 		WebElement event = cep.getEventname();
 		event.sendKeys(EVENTNAME);
 		event.sendKeys(Keys.PAGE_DOWN);
 
-		//Set the start date and end date
-		//WebElement timedropdown = cep.getPmdropdown();
-		//wutil.select(timedropdown, TIME);
-		
+		// Set the start date and end date
+		// WebElement timedropdown = cep.getPmdropdown();
+		// wutil.select(timedropdown, TIME);
+
 		// enter the start date
 		String STARTDATE = jLib.getRequriedDateYYYYDDMM(3);
 		System.out.println(STARTDATE);
 		cep.getEventstartdate().clear();
 		cep.getEventstartdate().sendKeys(STARTDATE);
-		UtilityClassObject.getTest().log(Status.PASS,"start date is displayed");
-		
-		//enter the end date
+		UtilityClassObject.getTest().log(Status.PASS, "start date is displayed");
+
+		// enter the end date
 		String ENDDATE = jLib.getRequriedDateYYYYDDMM(30);
 		System.out.println(ENDDATE);
 		cep.getEventenddate().clear();
 		cep.getEventenddate().sendKeys(ENDDATE);
 		UtilityClassObject.getTest().log(Status.PASS, "end date is displayed");
-		
-		//click on save button
+
+		// click on save button
 		cep.getSavebutton().click();
 		UtilityClassObject.getTest().log(Status.PASS, "events is saved");
-		//System.out.println("events is saved");
-		
-		
-		
+		// System.out.println("events is saved");
 
-		
-		
-	   
-		
-		
-		
-		
 	}
 
 }
